2c062bd6d1fe1865c1c5a7319eb9135f399a2c01
ol4juwon <olajuwonlawal2012@gmail.com>
2022-11-10 12:00:17 +0100

    sockets

--- app.js
+++ app.js
@@ -2,91 +2,40 @@
 require("dotenv").config({});
 const createError = require('http-errors');
 const http = require('http');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+// const path = require('path')
 const debug = require("debug")("app:app");
 let express = require('express');
 let app = express();
-const server = http.createServer(app);
-const moment = require("moment");
-global.isProduction = process.env.NODE_ENV == "production";
+// const moment = require("moment");
 require("./app/helper");
 
 require("express-async-errors");
 
 require("./startups")(app, express);
+const server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    debug('a user is connected', socket.id)
 
-app.use((req, res, next) => {
-    let requestId = getTimestamp();
-    console.log("Time Started", moment().toISOString(true), "headers", req.headers);
-    let url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log("Response",requestId);
-    debug(`[${requestId}] Request ${url}`, {
-        type: "request-response",
-        requestId,
-        body: req.body,
-        query: req.query,
-        params: req.params,
-        header: req.headers
-    });
+    socket.on('newTask', (message) => {
+        console.log('message from', message);
+        send('newTask', message)
+    })
+})
 
-    const cleanup = () => {
-        res.removeListener('finish', logFn);
-        res.removeListener('close', abortFn);
-        res.removeListener('error', errorFn)
-    };
+app.use(express.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', (req,res) => {
+try{
+    res.render('index.html')
 
-    const logFn = (a,b,c) => {
-        console.log("Response", requestId);
-        console.log("Time Ended", moment().toISOString(true));
-        cleanup();
-        debug( `[${requestId}] Response ${url}`,`${res.statusCode} ${res.statusMessage};`, {
-            type: "request-response",
-            body: req.body,
-            query: req.query,
-            params: req.params,
-            header: req.headers,
-            result: res.success || res.error,
-            code: res.statusCode,
-            client: res.user
-        })
-    };
-
-    const abortFn = () => {
-        cleanup();
-        console.log("Time Ended", moment().toISOString(true));
-       debug( `Response ${url}`,`${res.statusCode} ${res.statusMessage}; ${res.get('Content-Length') || 0}b sent || Request aborted by the client`, {
-            type: "request-response",
-            body: req.body,
-            query: req.query,
-            params: req.params,
-            header: req.headers,
-            result: res.success || res.error,
-            code: res.statusCode
-        });
-    };
-
-    const errorFn = err => {
-        cleanup();
-        console.log("Time Ended Error", moment().toISOString(true));
-        debug( `Response ${url}`,`${res.statusCode} ${res.statusMessage}; ${res.get('Content-Length') || 0}b sent || Request pipeline error: ${err}`, {
-            type: "request-response",
-            body: req.body,
-            query: req.query,
-            params: req.params,
-            header: req.headers,
-            result: res.success || res.error,
-            code: res.statusCode
-        });
-    };
-
-    res.on('finish', logFn); // successful pipeline (regardless of its response)
-    res.on('close', abortFn); // aborted pipeline
-    res.on('error', errorFn); // pipeline internal error
-    return next();
-});
-//routes
-
-
+}catch(err){
+    console.log("ll ", err.message)
+    res.send("ok")
+}
+})
 app.use("/api/v1", require("./routes/v1"));
 
 // catch 404 and forward to error handler
@@ -98,7 +47,7 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
-    console.log("error", {err});
+    console.log("error", err.message);
     debug(err.message, err, {
         request: {
             url: req.url|| {} ,
@@ -112,4 +61,8 @@ app.use((err, req, res, next) => {
     res.send({error: err && err.message || "An error occurred"});
 });
 
-module.exports = app;
+// module.exports = app;
+
+server.listen(5454, () => {
+    console.log('listening')
+})
--- workers/index.js
+++ workers/index.js
@@ -1,7 +1,8 @@
 const queue = require('../startups/queue');
 const Tasks =  require('../app/v1/tasks/TasksModel');
 const handleIncoming = (message) =>{
-const task = new Tasks(message.text);
+  console.log("handling new task",message)
+const task = new Tasks(message);
 task.save();}
 queue
   .receive('newTask', handleIncoming)
