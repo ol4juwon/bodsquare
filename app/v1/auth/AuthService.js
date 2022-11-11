29bb1ae4c920a671f194563d94d5e0249cb1d174
ol4juwon <olajuwonlawal2012@gmail.com>
2022-11-11 23:37:43 +0100

    sockets

--- app/v1/auth/AuthService.js
+++ app/v1/auth/AuthService.js
@@ -8,11 +8,13 @@ exports.login = async ({email, password}) => {
     if (!user) return { error: "Invalid email or password" };
     const validPass = await bcrypt.compare(password, user.password);
     if (!validPass) return { error: "Invalid password, try again" };
+    console.log(user)
     const token = jwt.sign(
 		{
 			
 			email: user.email,
             name: user.name,
+            user_id: user._id
 		},
 		process.env.TOKEN_SECRET,
 		{
--- startups/sockets.js
+++ startups/sockets.js
@@ -1,5 +1,6 @@
 const debug = require("debug")("app:app");
-module.exports = (server) => {
+const authservice = require('../app/v1/auth/AuthService')
+module.exports =async (server) => {
     var io = require('socket.io')(server,  {
         cors: {
         origins: 'localhost:3000'}
@@ -7,13 +8,27 @@ module.exports = (server) => {
     io.on('connection', (socket) => {
         debug('a user is connected', socket.id)
     socket.emit('welcome','ola');
-    socket.on('newUser', () => {
-    console.log('new ')
-    })
+        socket.on('newUser', () => {
+        console.log('new ')
+        })
         socket.on('newTask', (message) => {
             console.log('message from', message);
             send('newTask', message)
-            socket.emit('done',message)
+            socket.emit('newTaskList',message)
+        })
+        socket.on('updateTask', (task) => {
+            console.log('task', task)
+        })
+        socket.on('login',async ({loginData})=> {
+            console.log('logindata',loginData )
+            const {email, password } = loginData;
+            const {error, data} = await authservice.login({email, password})
+            console.log('error', error,'data', data);
+            if(data)
+            socket.emit('loginSuccess', data)
+            if(error)
+            socket.emit('loginError', error);
+
         })
     
     })
